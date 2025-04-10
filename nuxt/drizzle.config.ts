7f94dbfc3d676ba7a815b3920dd3552efa60b1ae
dech <aloyassskop@gmail.com>
2025-04-10 22:00:11 +0400

    idk

--- nuxt/drizzle.config.ts
+++ nuxt/drizzle.config.ts
@@ -5,6 +5,5 @@ export default defineConfig({
 	out: './migrations',
 	dialect: 'postgresql',
 	dbCredentials: {
-		url: process.env.NUXT_POSTGRES_URL!
-	},
+		url: process.env.NUXT_POSTGRES_URL!	},
 });
